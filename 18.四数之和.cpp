d552461baf6e87151109d79817210e2c70499664
Silhouette <silhouette20040723@sjtu.edu.cn>
2023-08-24 09:18:21 +0800

    wrong try of 18.fourSum

--- 18.四数之和.cpp
+++ 18.四数之和.cpp
@@ -10,12 +10,29 @@ class Solution
 public:
     vector<vector<int>> fourSum(vector<int> &nums, int target)
     {
-        unordered_map<int, int> map;
+        vector<vector<int>> quadruplets;
+        typedef pair<int, int> Wkij;
+        unordered_map<int, Wkij> map;
         sort(nums.begin(), nums.end());
-        n = nums.size();
+        int n = nums.size();
         for (int i = 0; i < n; i++)
             for (int j = i + 1; j < n; j++)
-                map[i, j] = nums[i] + nums[j];
+                map[nums[i] + nums[j]] = {i, j};
+        for (auto it = map.begin(); it != map.end(); it++)
+        {
+            if (map.count(target - it->first) != 0)
+            {
+                auto jt = map.find(target - it->first);
+                Wkij itW = it->second, jtW = jt->second;
+                int i = itW.first;
+                int j = itW.second;
+                int left = jtW.first;
+                int right = jtW.second;
+                if (i < left)
+                    quadruplets.push_back({nums[i], nums[j], nums[left], nums[right]});
+            }
+        }
+        return quadruplets;
     }
 };
 // @lc code=end
